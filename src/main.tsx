d75018a1a777e069a4990587265991929225fb15
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 14:11:49 -0400

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Root from './routes/root'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
 
+import { About } from 'remoteApp/About'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
---
+++ src/routes/root.tsx
@@ -0,0 +1,8 @@
+const Root = ()  => {
+    return( <div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </div>)
+}
+
+export default Root;
