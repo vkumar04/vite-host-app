311b73a0286c7a6e398421d75dd6c01435eda842
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 17:15:37 -0400

    added content to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { Suspense } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
-// @ts-ignore
-import { Button } from 'remoteApp/Button'
 import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <div>
-        <img src={reactLogo} alt="react logo" />
-        <h1>Vite Federated Host</h1>
-        <Suspense fallback="Loading Page">
-          <Button name="Button from Remote" />
-        </Suspense>
-        </div>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { Suspense } from 'react'
+import reactLogo from '../assets/react.svg'
+// @ts-ignore
+import { Button } from 'remoteApp/Button'
+
 export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <div>
+        <img src={reactLogo} alt="react logo" />
+        <h1>Vite Federated Host</h1>
+        <Suspense fallback="Loading Page">
+          <Button name="Button from Remote" />
+        </Suspense>
+        </div>
     </div>
   );
 };
