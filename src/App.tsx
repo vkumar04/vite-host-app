59376a6ce08464f434748b18157700720bde50be
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 09:16:39 -0400

    removed useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import {Button} from './components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
