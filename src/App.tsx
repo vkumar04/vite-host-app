655bfc94080482b7994a548d151e76b6eeb810f7
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 09:17:18 -0400

    changed title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div>
         <img src={reactLogo} alt="react logo" />
-        <h1>React + TypeScript + Vite</h1>
+        <h1>Vite Federated Host</h1>
         <Button name='Host Button' onClick={() => console.log('clicked')} />
         </div>
     </div>
