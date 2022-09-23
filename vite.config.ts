3d1bb431174d1d757b537273a780955558647c52
Vick Kumar <Vick.Kumar44@gmail.com>
2022-09-23 08:59:29 -0400

    added federation to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      shared: ['react', 'react-dom'],
+    }),
+  ]
 })
